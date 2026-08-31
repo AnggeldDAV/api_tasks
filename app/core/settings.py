0b23911dc49f1075a4ee1d31e2c6f9a009451e1b
AnggeldDAV <anggeld.adav@gmail.com>
2026-08-31 17:42:09 +0200

    Conf settings fixed

--- app/core/settings.py
+++ app/core/settings.py
@@ -8,7 +8,10 @@ class Settings(BaseSettings):
     ALGORITHM: str 
     
     
-    model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file="../.env", 
+                                      env_file_encoding="utf-8", 
+                                      extra="ignore", 
+                                      env_ignore_empty=True )
 
 @lru_cache        
 def get_settings():
