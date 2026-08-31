14dc049f790d92bfe73a1a398500dcad56c7b703
AnggeldDAV <anggeld.adav@gmail.com>
2026-08-31 17:53:20 +0200

    database creation on render

--- app/deps/deps.py
+++ app/deps/deps.py
@@ -7,9 +7,11 @@ from core.security import decodeJWT
 import jwt
 from crud.user import search_user
 from db.database import get_session_factory
+from db.database import Base,engine
 
 def get_db(settings:Settings = Depends(get_settings)):
     session_factory = get_session_factory(settings.DATABASE_URL)
+    Base.metadata.create_all(bind=engine)
     db = session_factory()
     try:
         yield db
