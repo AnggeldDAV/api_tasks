2474aa1920e625406cc5e269cea007accfe8340c
AnggeldDAV <anggeld.adav@gmail.com>
2026-08-31 19:17:44 +0200

    Add CD workflow for deploy after tests

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter,Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from schemas.auth import UserLogin
 from schemas.token import Token
@@ -9,11 +10,11 @@ from models.user import User
 api_router = APIRouter()
 
 @api_router.post("/login", response_model=Token)
-def login(login_user:UserLogin,db:Session = Depends(get_db)):
-    user = db.query(User).filter(User.name == login_user.username).first()
+def login(form_data: OAuth2PasswordRequestForm = Depends(),db:Session = Depends(get_db)):
+    user = db.query(User).filter(User.name == form_data.username).first()
     if user is None:
         raise HTTPException(status_code=401, detail="Incorrect username or password")
-    verify_pass = verify_password(login_user.password,user.hashed_password)
+    verify_pass = verify_password(form_data.password,user.hashed_password)
     if not verify_pass:
         raise HTTPException(status_code=401, detail="Incorrect username or password")
     token = signJWT(user.id)
--- app/deps/deps.py
+++ app/deps/deps.py
@@ -17,7 +17,7 @@ def get_db(settings:Settings = Depends(get_settings)):
     finally:
         db.close()
        
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
  
 def get_current_user(token:str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     cred_exep = HTTPException(
