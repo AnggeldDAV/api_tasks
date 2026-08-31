48116cb6544c7247a02c1559e950b6f3ef559488
AnggeldDAV <anggeld.adav@gmail.com>
2026-08-31 17:25:29 +0200

    Fix db bug creation

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -2,5 +2,5 @@ from db.database import Base,engine
 from models.user import *
 from models.task import *
 
-Base.metadata.create_all(bind=engine())
-print(f"Bases creadas correctamente")
+Base.metadata.create_all(bind=engine)
+print(f"Database created")
